b44c6e8a7cc55225c031e7486b0e80c6e58bf179
Martin <martin.huvelle@epita.fr>
2020-10-02 14:56:19 +0200

    add ressources

---
+++ ex3/poop-script.js
@@ -0,0 +1,28 @@
+function walk(node) {
+    var child, next;
+    var tagName = node.tagName ? node.tagName.toLowerCase() : "";
+    if (tagName == 'input' || tagName == 'textarea') {
+        return;
+    }
+    if (node.classList && node.classList.contains('ace_editor')) {
+        return;
+    }    switch (node.nodeType) {
+        case 1: // Element
+        case 9: // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while (child) {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;        case 3: // Text node
+            handleText(node);
+            break;
+    }
+}function handleText(textNode) {
+    var v = textNode.nodeValue;
+    v = v.replace(/\b\w\b/g, "💩");
+    textNode.nodeValue = v;
+}
+walk(document.body);
---
+++ ex3/rick-script.js
@@ -0,0 +1,1 @@
+document.querySelectorAll("a").forEach(a => a.href="https://youtu.be/dQw4w9WgXcQ")
