421fc4fe30bd42e01e4307777285120f4dc40726
Martin <martin.huvelle@epita.fr>
2020-10-02 16:23:23 +0200

    rename ad-hoc scripts

--- ex0/client.py
+++ ex0/client.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 
-
 import socket
+import time
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-client.bind(("", 37020))
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+# Set a timeout so the socket does not block
+# indefinitely when trying to receive data.
+server.settimeout(0.2)
+server.bind(("", 44444))
+message = b"your very important message"
 while True:
-    data, addr = client.recvfrom(1024)
-    print("received message: %s"%data)
+    server.sendto(message, ('192.168.1.102', 37020))
+    print("message sent!")
+    time.sleep(0.1)
--- ex0/server.py
+++ ex0/server.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 
-import socket
-import time
 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-# Set a timeout so the socket does not block
-# indefinitely when trying to receive data.
-server.settimeout(0.2)
-server.bind(("", 44444))
-message = b"your very important message"
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+client.bind(("", 37020))
 while True:
-    server.sendto(message, ('192.168.1.102', 37020))
-    print("message sent!")
-    time.sleep(0.1)
+    data, addr = client.recvfrom(1024)
+    print("received message: %s"%data)
