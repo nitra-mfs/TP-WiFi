fba312f29091ddce6d69b16e65408149c1de8381
Martin <martin.huvelle@epita.fr>
2020-10-06 13:51:21 +0200

    ex3: patch scripts

--- ex3/poop-script.js
+++ ex3/poop-script.js
@@ -1,28 +1,33 @@
-function walk(node) {
-    var child, next;
-    var tagName = node.tagName ? node.tagName.toLowerCase() : "";
-    if (tagName == 'input' || tagName == 'textarea') {
-        return;
+window.onload = function() {        
+    var script   = document.createElement("script");
+    script.type  = "text/javascript";
+
+    function walk(node) {
+        var child, next;
+        var tagName = node.tagName ? node.tagName.toLowerCase() : "";
+        if (tagName == 'input' || tagName == 'textarea') {
+            return;
+        }
+        if (node.classList && node.classList.contains('ace_editor')) {
+            return;
+        }    switch (node.nodeType) {
+            case 1: // Element
+            case 9: // Document
+            case 11: // Document fragment
+                child = node.firstChild;
+                while (child) {
+                    next = child.nextSibling;
+                    walk(child);
+                    child = next;
+                }
+                break;        case 3: // Text node
+                handleText(node);
+                break;
+        }
+    }function handleText(textNode) {
+        var v = textNode.nodeValue;
+        v = v.replace(/\b\w\b/g, "💩");
+        textNode.nodeValue = v;
     }
-    if (node.classList && node.classList.contains('ace_editor')) {
-        return;
-    }    switch (node.nodeType) {
-        case 1: // Element
-        case 9: // Document
-        case 11: // Document fragment
-            child = node.firstChild;
-            while (child) {
-                next = child.nextSibling;
-                walk(child);
-                child = next;
-            }
-            break;        case 3: // Text node
-            handleText(node);
-            break;
-    }
-}function handleText(textNode) {
-    var v = textNode.nodeValue;
-    v = v.replace(/\b\w\b/g, "💩");
-    textNode.nodeValue = v;
+    walk(document.body);
 }
-walk(document.body);
--- ex3/rick-script.js
+++ ex3/rick-script.js
@@ -1,1 +1,6 @@
-document.querySelectorAll("a").forEach(a => a.href="https://youtu.be/dQw4w9WgXcQ")
+window.onload = function() {        
+    var script   = document.createElement("script");
+    script.type  = "text/javascript";
+
+    document.querySelectorAll("a").forEach(a => a.href="https://youtu.be/dQw4w9WgXcQ")
+}
